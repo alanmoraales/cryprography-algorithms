a5384cd9ae2b88df85e44ce714cdf98fd9ef6f1f
César González <chesarglez429@gmail.com>
2021-10-11 13:23:13 -0500

    :bug: Remove newlines in "write_file"

--- write_file.py
+++ write_file.py
@@ -1,4 +1,4 @@
 def write_file(file_dir, content):
-    file = open(file_dir, "w", encoding='utf-8')
+    file = open(file_dir, "w", encoding='utf-8', newline="")
     file.write(content)
     file.close()
