20ad2cba8c5ec454b1c9892faaf99bb3a354141e
César González <chesarglez429@gmail.com>
2021-10-11 14:24:39 -0500

    :recycle: Use better naming in exercise 6

--- exercise6.py
+++ exercise6.py
@@ -3,12 +3,14 @@ from algorithms.RSA import RSAAlgorithm
 from file_writer import FileWriter
 
 
-def encrypt_with_rsa():
+def desencrypt_with_rsa():
     rsa = RSAAlgorithm()
     FileWriter.transform_by_chunk(
-        RSA_ENCRYPTED_OUTPUT, RSA_DECRYPTED_OUTPUT, rsa.desencrypt
+        input_path=RSA_ENCRYPTED_OUTPUT,
+        output_path=RSA_DECRYPTED_OUTPUT,
+        parse=rsa.desencrypt,
     )
 
 
-[measured_time, _] = FileWriter.with_measure_time(encrypt_with_rsa)
+[measured_time, _] = FileWriter.with_measure_time(desencrypt_with_rsa)
 FileWriter.write_file(RSA_DECRYPTED_TIME, str(measured_time))
