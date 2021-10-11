d0d9619ac7f71306617cc0a8df7a5d486a744704
César González <chesarglez429@gmail.com>
2021-10-11 13:14:18 -0500

    :sparkles: Write rsa algorythms by chunks

--- exercise3.py
+++ exercise3.py
@@ -1,12 +1,13 @@
 from algorithms.RSA import RSAAlgorithm
 from constants import RSA_ENCRYPTED_OUTPUT, BIG_FILE_DIR, RSA_ENCRYPTED_TIME
 from with_measure_time import with_measure_time
+from write_by_chunk import write_by_chunk
 from write_file import write_file
 from write_line_by_line import write_line_by_line
 
 def encrypt_with_rsa():
     rsa = RSAAlgorithm()
-    write_line_by_line(BIG_FILE_DIR, RSA_ENCRYPTED_OUTPUT, rsa.encrypt)
+    write_by_chunk(BIG_FILE_DIR, RSA_ENCRYPTED_OUTPUT, rsa.encrypt, chunk_size=100)
     
 [measured_time, _] = with_measure_time(encrypt_with_rsa)
 write_file(RSA_ENCRYPTED_TIME, str(measured_time))
--- exercise6.py
+++ exercise6.py
@@ -1,14 +1,12 @@
-from constants import RSA_ENCRYPTED_OUTPUT, RSA_DECRYPTED_TIME, RSA_DECRYPTED_OUTPUT, BIG_FILE_DIR
+from constants import RSA_ENCRYPTED_OUTPUT, RSA_DECRYPTED_TIME, RSA_DECRYPTED_OUTPUT
 from algorithms.RSA import RSAAlgorithm
 from with_measure_time import with_measure_time
+from write_by_chunk import write_by_chunk
 from write_file import write_file
 
-rsa = RSAAlgorithm()
-
-with open(BIG_FILE_DIR, "rb") as file:
-    data_to_encrypt = file.read()
-    encrypted_file = rsa.encrypt(data_to_encrypt)
-    [measured_time, decrypted_file] = with_measure_time(
-        lambda: rsa.desencrypt(encrypted_file))
-    write_file(RSA_DECRYPTED_TIME, str(measured_time))
-    write_file(RSA_DECRYPTED_OUTPUT, decrypted_file.decode('utf-8'))
+def encrypt_with_rsa():
+    rsa = RSAAlgorithm()
+    write_by_chunk(RSA_ENCRYPTED_OUTPUT, RSA_DECRYPTED_OUTPUT, rsa.desencrypt)
+    
+[measured_time, _] = with_measure_time(encrypt_with_rsa)
+write_file(RSA_DECRYPTED_TIME, str(measured_time))
