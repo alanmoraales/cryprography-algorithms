82f31a9a4be9acfe03d0bad8a4a45c24211115ea
César González <chesarglez429@gmail.com>
2021-10-11 12:13:13 -0500

    :sparkles: Apply algorythm line by line

--- exercise3.py
+++ exercise3.py
@@ -4,11 +4,15 @@ from with_measure_time import with_measure_time
 from write_file import write_file
 
 rsa = RSAAlgorithm()
+rsa_input = open(BIG_FILE_DIR, mode="rb")
+rsa_output = open(RSA_ENCRYPTED_OUTPUT, mode="w", encoding="utf-8")
 
-with open(BIG_FILE_DIR, "rb") as file:
-    data_to_encrypt = file.read()
-    data = file.read()
-    [measured_time, encrypted_file] = with_measure_time(
-        lambda: rsa.encrypt(data_to_encrypt))
-    write_file(RSA_ENCRYPTED_TIME, str(measured_time))
-    write_file(RSA_ENCRYPTED_OUTPUT, encrypted_file.hex())
+def write_stuff():
+    for data_to_encrypt in rsa_input:
+        encrypted_data = rsa.encrypt(data_to_encrypt)
+        rsa_output.write(encrypted_data.hex())
+
+rsa_output.close()
+
+[measured_time, _] = with_measure_time(write_stuff)
+write_file(RSA_ENCRYPTED_TIME, str(measured_time))
